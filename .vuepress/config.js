15f1b2fd2bf61c150b5279f1b257e2e040020088
yuangegena <yuangegena@163.com>
2020-07-08 15:52:00 +0800

    初始化vuepress_blog

---
+++ .vuepress/config.js
@@ -0,0 +1,99 @@
+module.exports = {
+  title: "yuangegena_blog",
+  description: 'welcome to my blog',
+  dest: 'public',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  theme: 'reco',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/', icon: 'reco-home' },
+      { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+      { text: 'Docs', 
+        icon: 'reco-message',
+        items: [
+          { text: 'vuepress-reco', link: '/docs/theme-reco/' }
+        ]
+      },
+      { text: 'Contact', 
+        icon: 'reco-message',
+        items: [
+          { text: 'Gitee', link: 'https://gitee.com/yuangegena/dashboard/projects', icon: 'reco-github' }
+        ]
+      }
+    ],
+    sidebar: {
+      '/docs/theme-reco/': [
+        '',
+        'theme',
+        'plugin',
+        'api'
+      ]
+    },  
+    type: 'blog',
+    // 博客设置
+    blogConfig: {
+      category: {
+        location: 2, // 在导航栏菜单中所占的位置，默认2
+        text: 'Category' // 默认 “分类”
+      },
+      tag: {
+        location: 3, // 在导航栏菜单中所占的位置，默认3
+        text: 'Tag' // 默认 “标签”
+      }
+    },
+    friendLink: [
+      {
+        title: '码云',
+        desc: '欢迎关注码云',
+        email: 'yuangegena@163.com',
+        link: 'https://gitee.com/yuangegena/dashboard/projects'
+      },
+      {
+        title: 'github',
+        desc: '欢迎关注github',
+        email: 'yuangegena@163.com',
+        link: 'https://gitee.com/yuangegena/dashboard/projects'
+      },
+    ],
+    logo: '/logo.png',
+    // 搜索设置
+    search: true,
+    searchMaxSuggestions: 10,
+    // 自动形成侧边导航
+    // sidebar: 'auto',
+    // 最后更新时间
+    lastUpdated: 'Last Updated',
+    // 作者
+    author: 'yuangegena',
+    // 作者头像
+    authorAvatar: '/avatar.png',
+    // 备案号
+    // record: 'xxxx',
+    // 项目开始时间
+    startYear: '2020'
+    /**
+     * 密钥 (if your blog is private)
+     */
+
+    // keyPage: {
+    //   keys: ['your password'],
+    //   color: '#42b983',
+    //   lineColor: '#42b983'
+    // },
+
+    /**
+     * valine 设置 (if you need valine comment )
+     */
+
+    // valineConfig: {
+    //   appId: '...',// your appId
+    //   appKey: '...', // your appKey
+    // }
+  },
+  markdown: {
+    lineNumbers: true
+  }
+}  
